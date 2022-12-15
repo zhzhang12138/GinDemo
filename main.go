3df75ffe78a5e8b70b8563aa1d0850804999f192
张涛 <zhzhang12138@163.com>
2022-12-15 14:28:42 +0800

    create endless实现不停机重启 Go 程序

--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/fvbock/endless"
+	"github.com/gin-gonic/gin"
+	"strconv"
+	"time"
+)
 
-func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+// Routers 初始化路由
+func Routers() *gin.Engine {
+	Router := gin.Default()
+
+	Router.GET("/ping", func(c *gin.Context) {
+		sleepTime, _ := strconv.Atoi(c.Query("time"))
+		time.Sleep(time.Duration(sleepTime) * time.Second)
+		//fmt.Println(123456789)
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "sleep---" + c.Query("time") + "s",
 		})
 	})
-	r.Run(":8888") // listen and serve on 0.0.0.0:8080
+
+	return Router
+}
+
+// 初始化服务
+func initServer(address string, router *gin.Engine) {
+	s := endless.NewServer(address, router)
+	s.ReadHeaderTimeout = 100 * time.Second // 该参数表示读取请求头的最长时间。
+	s.WriteTimeout = 100 * time.Second
+	s.MaxHeaderBytes = 1 << 20
+	// 使用 endless.ListenAndServe 方法启动 HTTP 服务器
+	s.ListenAndServe()
+}
+
+func main() {
+	// endless实现不停机重启 Go 程序
+	// 参考文章：https://www.luozhiyun.com/archives/584
+	Router := Routers()
+	initServer(":8080", Router)
 }
